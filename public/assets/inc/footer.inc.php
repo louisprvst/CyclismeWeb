4a879c36ea60a766e82d84dc1934841e9676fb3f
lucas210905 <lucas.debruyne2109@gmail.com>
2025-04-01 14:51:41 +0200

    ajout header footer

---
+++ public/assets/inc/footer.inc.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cyclisme Web</title>
+    <link rel="stylesheet" href="CyclismeWeb\public\assets\styles\styles.css">
+</head>
+<footer>
+    <div class="logo_footer">
+        <img src="CyclismeWeb\public\assets\inc\img\Tour_de_France_logo.svg.png" alt="Logo_tdf">
+    </div>
+    <div class="footer_links">
+        <a href="#COPYRIGHT">Copyright : © 2025 Prono Cyclisme</a><br>
+        <a href="#MENTIONS_LÉGALES">Mentions légales & politique de confidentialité</a><br>
+        <a href="#CRÉDITS">Crédits</a>
+    </div>
+</footer>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../controllers/indexController.php';
+//require_once __DIR__ . '/../controllers/indexController.php';
 ?>
 
 <!DOCTYPE html>
@@ -7,9 +7,20 @@ require_once __DIR__ . '/../controllers/indexController.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Cyclisme Web</title>
+    <link rel="stylesheet" href="CyclismeWeb\public\assets\styles\styles.css">
 </head>
 <body>
-    <h1>Bienvenue sur la page d'accueil</h1>
+    <div id="header-container">
+        <iframe src="CyclismeWeb\public\assets\inc\header.inc.php" frameborder="0" style="width: 100%; height: auto;"></iframe>
+    </div>
+    <main>
+        <div class="container">
+            <img src="CyclismeWeb\public\assets\inc\img\tdf25-affiche-parcours-1.jpg" alt="Carte tdf" style="width: 100%; height: auto;">
+        </div>
+    </main>
+    <div id="footer-container">
+        <iframe src="CyclismeWeb\public\assets\inc\footer.inc.php" frameborder="0" style="width: 100%; height: auto;"></iframe>
+    </div>
 </body>
 </html>
