7789abc0036d3f017b25d66545f3586b8ed22168
Mathéïs Fardel <matheis.fardel@orange.fr>
2025-04-01 10:46:07 +0200

    Init Commit (réglages serveur)

---
+++ config/bdd.conf.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php'; 
+
+use Dotenv\Dotenv;
+
+// Charger les variables d'environnement depuis le fichier .env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Initialiser la connexion à la base de données
+try {
+    $dsn = "mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'];
+    $bdd = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Vous pouvez utiliser $pdo pour vos requêtes SQL
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
---
+++ public/controllers/indexController.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../../config/init.conf.php';
+
+// // Vérifier si l'utilisateur est connecté
+// if (myAuthClass::is_auth($_SESSION)) {
+//     $user = $_SESSION['user']; // Récupérer les informations de l'utilisateur connecté
+// } else {
+//     $user = null; // Pas d'utilisateur connecté
+// }
